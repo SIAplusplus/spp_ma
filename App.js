2d64f5f65be74ba59c27e6a7f3debbf2e44708be
izarater <izarater@unal.edu.co>
2022-10-28 19:35:32 -0500

    saving erased first pseudoversion

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Login from './src/components/login';
+import Routes from './src/navigation/Routes';
+import Main from './src/components/main';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main></Main>
   );
 }
 
